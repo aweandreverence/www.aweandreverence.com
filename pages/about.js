87209b396e21916252bd430235b466ee7ec81988
Elijah Ang <eang0521@users.noreply.github.com>
2020-11-24 05:47:33 +0000

    added image to about page

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,8 @@ import Page from '../components/base_page';
 
 import css from '../styles/common.module.scss';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function About() {
     return (
@@ -12,7 +14,10 @@ export default function About() {
 
             <br />
 
-            <div className={css.card}>
+            <Row>
+            <Col>
+            
+            <div className={css.textleft}><div className={css.card}>
                 
                 <h3>Vision</h3>
                 <p>An increasingly digitally connected world brings opportunity to give more access to the Christian gospel and resources to help them live out their faith. Awe & Reverence brings modern technology to an ancient faith. While the Christian life consists of interdependent practices, such as prayer, worship, Bible study, and meditation, the utilization of advanced technology to aid these practices in a harmonious way does not exist. Additionally, world-wide access to Christian worship and study resources in multiple languages is lacking.</p>
@@ -22,7 +27,12 @@ export default function About() {
                 <p>Awe & Reverence is building a world-class ecosystem of meaningful applications designed to facilitate and enhance the practice of the Christian faith. We are doing that by building individual applications focused on particular aspects of Christian worship, while keeping an overall vision to ensure that these individual apps seamlessly integrate within the A&R ecosystem. We will achieve a cohesiveness that has never been done before by utilizing metadata, data science, natural language processing, and other advanced logic. Simply put, we are applying the most advanced technologies to encourage rich and deep worship of the God who is deserving of all of our worship. And we're doing this in as many languages as possible!</p>
                 
                 <br />
-            </div>
+            </div></div>
+            </Col>
+            <Col>
+                <img src="https://images.unsplash.com/photo-1476041026529-411f6ae1de3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1300&q=80" className={css.right} />
+            </Col>
+            </Row>
         </Page>
     );
 }
