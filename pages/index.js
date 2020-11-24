093a3ea045834367dd2e0061e02fe500c9c66e03
Elijah Ang <eang0521@users.noreply.github.com>
2020-11-24 05:10:19 +0000

    added second image to home page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Page from '../components/base_page';
 import css from '../styles/common.module.scss';
 
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Home() {
     return (
@@ -18,14 +20,17 @@ export default function Home() {
             </div>
 
             <br />
-        
-            <div className={css.card}>
-                <h3>Apprenticeship</h3>
-                <p>Our founder, Jonathan Tsai, started an apprenticeship program at A&R to train and equip aspiring Christian software engineers with tangible skills. In the SF Bay Area there is an ever-increasing number of people coming out of coding bootcamps trying to transition into software roles from other industries. Unfortunately, opportunities for bootcamp grads have severely declined since 2015 and many people are struggling to find employment after investing substantially into building their coding skills...</p>
-                <br />
-                <Button href="/apprenticeship" variant="outline-dark">Learn More</Button>
-            </div>
-            
+            <Row>
+            <Col className={css.cardimage}><img src="https://images.unsplash.com/photo-1511162862207-a0857d04c7b6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" /></Col>
+                <Col>
+                    <div className={css.card}>
+                        <h3>Apprenticeship</h3>
+                        <p>Our founder, Jonathan Tsai, started an apprenticeship program at A&R to train and equip aspiring Christian software engineers with tangible skills. In the SF Bay Area there is an ever-increasing number of people coming out of coding bootcamps trying to transition into software roles from other industries. Unfortunately, opportunities for bootcamp grads have severely declined since 2015 and many people are struggling to find employment after investing substantially into building their coding skills...</p>
+                        <br />
+                        <Button href="/apprenticeship" variant="outline-dark">Learn More</Button>
+                    </div>
+                </Col>
+            </Row>
             <hr />
 
             <div className={css.card}>
