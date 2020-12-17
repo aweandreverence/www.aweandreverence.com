0f30b75bc64baafd46cbca192e4047b9231b778c
Elijah Ang <eang0521@users.noreply.github.com>
2020-12-17 05:42:43 +0000

    added bolded navbar

--- components/header.js
+++ components/header.js
@@ -1,36 +1,27 @@
+import classNames from 'classnames';
+import Nav from 'react-bootstrap/Nav';
 import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 import css from '../styles/header.module.scss';
+import nav_links from './nav_links.js';
 
-import Nav from 'react-bootstrap/Nav';
-//import NavItem from 'react-bootstrap/NavItem';
-//import NavLink from 'react-bootstrap/NavLink';
-
-//navData = [
-//    {page: "Home", path: "/"},
-//    {page: "About", path: "/about"},
-//    {page: "Apprenticeship", path: "/apprenticeship"}
-//];
-
-
-
-function Header({ children }) {
+function Header({children}) {
+    const router = useRouter();
+    const navItemsJSX = nav_links.map((link, index) => {
+        const classes = classNames({
+            [css.active]: link.url === router.pathname
+        });
+        const target = link.url.substring(0, 4) === 'http' ? '_blank' : null;
+        return (
+            <Nav.Item key={`link-${index}`}>
+                <Nav.Link href={link.url} className={classes} target={target}>{link.name}</Nav.Link>
+            </Nav.Item>
+        );
+    });
     return (
         <div className={css.header}>
-            <Nav>
-                <Nav.Item>
-                    <Nav.Link href="/">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link href="https://maskil.church" target="_blank">Maskil</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link href="/about">About</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link href="/apprenticeship">Apprenticeship</Nav.Link>
-                </Nav.Item>
-            </Nav>
+            <Nav>{navItemsJSX}</Nav>
         </div>
     );
 }
---
+++ components/nav_links.js
@@ -0,0 +1,20 @@
+const nav_links = [
+    {
+        'name': 'Home',
+        'url': '/'
+    },
+    {
+        'name': 'Maskil',
+        'url': 'https://maskil.church'
+    },
+    {
+        'name': 'About',
+        'url': '/about'
+    },
+    {
+        'name': 'Apprenticeship',
+        'url': '/apprenticeship'
+    }
+];
+
+export default nav_links;
