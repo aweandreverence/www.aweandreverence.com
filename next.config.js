d1fcb1b50d61ceeb52884d6cc8e46c415b546a51
Jonathan Tsai <jontsai@aweandreverence.com>
2023-11-14 12:27:22 -0800

    update Next config to export on build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 // next.config.js
 
-
 module.exports = {
     /* config options here */
+    // https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+    output: 'export',
 };
